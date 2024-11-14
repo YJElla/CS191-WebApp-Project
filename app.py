b570ae0f97d55fa6c0e0f0bb93cbf57880761762
Yanni Jose Ella <ycella@up.edu.ph>
2024-11-14 16:42:32 +0800

    added login credentials

--- app.py
+++ app.py
@@ -1,21 +1,48 @@
 import os
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session, flash
 from werkzeug.utils import secure_filename
 import pytesseract
 from PIL import Image
 from ocr_pdf import extract_text_from_pdf  # Import the PDF processing function
+import mysql.connector
+
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'C:/Users/yanni/OneDrive/Desktop/1sr Sem [Year 3]/CS165/web_app' #Specify folder directory
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Limit file size to 16 MB
+
+app.secret_key = "my_secret_key" 
 
 # Ensure upload folder exists
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
 @app.route('/')
+def login():
+    return render_template('login_page.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    username = request.form['username']
+    password = request.form['password']
+
+    # Replace with actual usernames
+    if username == "username" and password == "password":  # Dummy credentials
+        session['user'] = username  # Store user in session
+        return redirect(url_for('index'))  # Redirect to upload page
+    else:
+        flash('Invalid credentials. Please try again.')
+        return redirect(url_for('login'))
+
+
+@app.route('/index')
 def index():
+    # Check if the user is logged in
+    if 'user' not in session:
+        return redirect(url_for('login'))
     return render_template('index.html')
+    
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -45,6 +72,22 @@ def extract_text_from_image(image_path):
     with Image.open(image_path) as img:
         text = pytesseract.image_to_string(img)
     return text
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status' : 404,
+        'message' : 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp 
 
 if __name__ == '__main__':
     app.run(debug=True)
